47ba6e6a876c15ecb74a1ff4584721eb64508866
Keith Gaughan <k@stereochro.me>
2023-07-21 08:52:09 +0100

    Clean up golden test driver

--- tests/golden/golden.sh
+++ tests/golden/golden.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
 set -eu
+
 here="$(dirname "$0")"
 for input in "$here"/*.in.xml; do
-	dest=$(mktemp ${TMPDIR:-/tmp}/tmp.golden.XXXXXXXX.xml)
 	golden="${input%%.in.xml}.out.xml"
+
+	dest="$(mktemp "${TMPDIR:-/tmp}"/tmp.golden.XXXXXXXX.xml)"
 	python3 -m xmlsafemod \
 		--set username "./uname" "./delegate/profile" \
 		--set password "./pwd" \
-		 --src "$input" --dest "$dest" < "$here"/creds.json
+		--src "$input" --dest "$dest" < "$here"/creds.json
+
 	if ! cmp -s "$dest" "$golden"; then
-		echo "Found different between golden file '$golden' and '$input' when processed:"
+		echo "Found difference between golden file '$golden' and '$input' when processed:"
 		diff -u "$golden" "$dest"
 		exit 1
 	fi
